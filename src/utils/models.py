1fd39e06d9a34d753f7226b921b6f451e9838bbd
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-05-17 11:49:20 +0700

    Update models.py
    added model customization in hidden layers

--- src/utils/models.py
+++ src/utils/models.py
@@ -1,21 +1,25 @@
 from torch import nn
 
 class MLPClassifier(nn.Module):
-    def __init__(self):
+    def __init__(self,
+        input_dim: int,
+        hidden_dims: tuple[int] = (512, 512, 512, 512),
+        activation_fn: nn.Module | None = None
+    ):
         super().__init__()
+        if activation_fn is None:
+           activation_fn = nn.LeakyReLU(.4)
 
-        self.relu6 = nn.ReLU6()
-        self.mlp = nn.Sequential(
-            nn.Linear(768 + 25, 512), nn.LeakyReLU(.4),
-            nn.BatchNorm1d(512),
-            nn.Linear(512, 512), nn.LeakyReLU(.4),
-            nn.BatchNorm1d(512),
-            nn.Linear(512, 512), nn.LeakyReLU(.4),
-            nn.BatchNorm1d(512),
-            nn.Linear(512, 512), nn.LeakyReLU(.4),
-            nn.BatchNorm1d(512),
-            nn.Linear(512, 1)
-        )
+        self.mlp = nn.Sequential()
+        for i, dim in enumerate(hidden_dims):
+            self.mlp.add_module(str(i),
+                nn.Sequential(
+                   nn.Linear(hidden_dims[i - 1] if i != 0 else input_dim, dim),
+                   activation_fn,
+                   nn.BatchNorm1d(dim)
+                )
+            )
+        self.mlp.add_module("linear_out", nn.Linear(hidden_dims[-1], 1))
 
     def forward(self, movie):
       logits = self.mlp(movie)
